141f38bb1d2a689c69cbb66df5ba3a806a12b96a
Mark Leigh David <it.markleighdavid@gmail.com>
2024-02-22 12:41:26 +0800

    refactor: removed-unnecessary-logout-code

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,18 +1,15 @@
 import useAxiosPrivate from './useAxiosPrivate';
 import useAuth from './useAuth';
-import { useNavigate } from 'react-router-dom';
 
 const useLogout = () => {
  const { setAuth } = useAuth();
  const axiosPrivate = useAxiosPrivate();
- const nav = useNavigate();
 
  const Logout = async () => {
   try {
     await axiosPrivate.get('/auth/logout')
       .then(() => {
         setAuth(null);
-        nav('/auth/login', { replace: true });
       });
   } catch (err) {
     throw new Error(err);
