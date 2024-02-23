4e488557d7e35170b6a211903967c6afa3b723fb
Mark Leigh David <it.markleighdavid@gmail.com>
2024-02-23 10:24:30 +0800

    feat: created-dynamic-modal

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import useAuth from "../hooks/useAuth"
 import useAxiosPrivate from "../hooks/useAxiosPrivate"
 import { useNavigate } from "react-router-dom";
+import Modal from "../components/Modal";
 
 const Dashboard = () => {
 	const { auth } = useAuth();
@@ -9,6 +10,10 @@ const Dashboard = () => {
 	const axiosPrivate = useAxiosPrivate();
 	const [users, setUsers] = useState([]);
 	const [request, setRequest] = useState(false);
+
+	const tryCallaback = () => {
+		console.log('callback works');
+	};
 
 	useEffect(() => {
 		if (!auth) nav('/auth/login');
@@ -29,24 +34,29 @@ const Dashboard = () => {
 
   return (
     <div>
-			<h1>Dashboard</h1>	
-			<div>
-				{
-					users.map((user, i) => {
-						return (
-							<div key={i}>
-								<h3>{user.username}</h3>
-								<p>{user.email}</p>
-							</div>
-						)
-					})
-				}
-			</div>
-			<button
+			<h1>Dashboard</h1>
+			<Modal
+				text='Show Users'
+				title='Users' 
+				callback={tryCallaback}>
+				<div>
+					{
+						users.map((user, i) => {
+							return (
+								<div key={i}>
+									<h3>{user.username}</h3>
+									<p>{user.email}</p>
+								</div>
+							)
+						})
+					}
+				<button
 				className="bg-blue-500 text-white px-4 py-2  hover:bg-blue-700 rounded-md"
 				onClick={() => setRequest(!request)}>
 				Refresh
-			</button>
+				</button>
+				</div>
+			</Modal>
 		</div>
   )
 };
